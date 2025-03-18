7df4b0be2d483dc9302318f63d1482c64627b132
elena <lin.elena@franchettisalviani.net>
2025-03-18 12:01:10 +0100

    es A

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Random r = new Random();
+void popolaVendite(int[,] vm)
+{
+    for (int i = 0; i < vm.GetLength(1); i++)
+    {
+        vm[0, i] = r.Next(100, 1001);  
+    }
+}
+
+int[,] vm = new int[2,12];
+popolaVendite(vm);
---
+++ Verifica fila A - es A/Program.cs
@@ -0,0 +1,24 @@
+﻿Random r = new Random();
+void popolaVendite(int[,] vm)
+{
+    for (int i = 0; i < vm.GetLength (1); i++)
+    {
+        vm[0, i] = r.Next(100,1001);
+    }
+}
+
+void stampa(int[,] vm)
+{
+    for(int i = 0;i < vm.GetLength(0); i++)
+    {
+        for(int j = 0;j < vm.GetLength(1); j++)
+        {
+            Console.Write($"\t{vm[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] vm = new int[2, 12];
+popolaVendite(vm);
+stampa(vm);
+
